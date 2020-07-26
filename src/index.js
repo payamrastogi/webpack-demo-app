fdf3871ce1df580bfc522008ef962f5e78677f14
Payam Rastogi <payamrastogi@gmail.com>
2020-07-26 17:11:12 -0400

    Cache busting and HtmlWebPack Plugin

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import {ComponentService} from "./app/component.service";
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
-
+console.log("HI");
 run(alertService, componentService);
