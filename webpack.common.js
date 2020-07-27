03df24109d549bfb6ec1691b48488816bf96a631
Payam Rastogi <payamrastogi@gmail.com>
2020-07-26 23:47:11 -0400

    Minify HTML, CSS, and JS in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,4 @@
 const path = require("path");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     //devtool: "none", //remove eval function from main.js
@@ -7,19 +6,8 @@ module.exports = {
         main: "./src/index.js",
         vendor: "./src/vendor.js"
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
     module: {
         rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    "style-loader", //3. Inject styles into DOM
-                    'css-loader', //2. Turns css into commonjs
-                    "sass-loader" //1. turns sass into css
-                ],
-            },
             {
                 test: /\.html$/,
                 use: ["html-loader"]
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: "production",
@@ -10,5 +14,34 @@ module.exports = merge(common, {
         filename: "[name].[contentHash].bundle.js", //cache-busting
         path: path.resolve(__dirname, "dist")
     },
-    plugins: [new CleanWebpackPlugin()]
+    optimization:{
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: "[name].[contentHash].css"}), 
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+            minify:{
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true
+            }
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, //3. Extract css into files
+                    'css-loader', //2. Turns css into commonjs
+                    "sass-loader" //1. turns sass into css
+                ],
+            },
+        ]
+    }
 });
